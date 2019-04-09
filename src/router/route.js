89207e2364a8628ddbca2f24e6d70fb5c822c2fd
stone-liu3432 <liushuang3432@163.com>
2019-04-09 11:05:29 +0800

    更新路由表

--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,19 @@
 // login
-const login = () => import(/* webpackChunkName: "main-page" */  '@/components/page/login')
+const login = () => import(/* webpackChunkName: "login-page" */  '@/components/page/login')
+
+//  main page
 const main = () => import(/* webpackChunkName: "main-page" */ '@/components/page/nmsMain')
+const home = () => import(/* webpackChunkName: "main-page" */ '@/components/content/home')
+
+//  sub page
+const alert = () => import(/* webpackChunkName: "alert" */ '@/components/content/alert')
+const system = () => import(/* webpackChunkName: "system" */ '@/components/content/system')
+const task = () => import(/* webpackChunkName: "task" */ '@/components/content/task')
+const performance = () => import(/* webpackChunkName: "perf" */ '@/components/content/perf')
+
+const topo = () => import(/* webpackChunkName: "device" */ '@/components/content/topo')
+const device = () => import(/* webpackChunkName: "device" */ '@/components/content/device')
+const onulist = () => import(/* webpackChunkName: "device" */ '@/components/content/onulist')
 
 const routes = [
     {
@@ -23,7 +36,63 @@ const routes = [
                 meta: {
                     requireAuth: true
                 },
-                component: main,
+                component: home,
+            },
+            {
+                path: "/home",
+                meta: {
+                    requireAuth: true
+                },
+                component: home,
+            },
+            {
+                path: "/alert",
+                meta: {
+                    requireAuth: true
+                },
+                component: alert,
+            },
+            {
+                path: "/device",
+                meta: {
+                    requireAuth: true
+                },
+                component: device,
+            },
+            {
+                path: "/onulist",
+                meta: {
+                    requireAuth: true
+                },
+                component: onulist,
+            },
+            {
+                path: "/system",
+                meta: {
+                    requireAuth: true
+                },
+                component: system,
+            },
+            {
+                path: "/task",
+                meta: {
+                    requireAuth: true
+                },
+                component: task,
+            },
+            {
+                path: "/topo",
+                meta: {
+                    requireAuth: true
+                },
+                component: topo,
+            },
+            {
+                path: '/performance',
+                meta: {
+                    requireAuth: true
+                },
+                component: performance
             }
         ]
     }
