4827eee86da815e3065226d0ac3579adbb22dade
stone-liu3432 <liushuang3432@163.com>
2019-04-09 11:06:36 +0800

    更新store

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import menu from '../config/menu.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
         isLoading: false,
+        menu,
     },
     mutations: {
         changeLoadingState(state, data){
