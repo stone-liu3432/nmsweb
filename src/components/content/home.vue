98370421cd72a3ddd997e2b04f2e3d27130d0baf
stone-liu3432 <liushuang3432@163.com>
2019-04-11 15:48:07 +0800

    添加图表demo

--- src/components/content/home.vue
+++ src/components/content/home.vue
@@ -1,49 +1,78 @@
 <template>
     <div>
-        <pie :id="id" :data="data" v-if="data.series"></pie>
+        <el-row :gutter="20">
+            <el-col :span="20">
+                <el-row :gutter="20">
+                    <el-col :span="12">
+                        <pie :data="data" :id="id" v-if="data"></pie>
+                    </el-col>
+                    <el-col :span="12">
+                        <line-charts :data="data1" :id="id1"></line-charts>
+                    </el-col>
+                </el-row>
+            </el-col>
+            <el-col :span="4">
+                <div style="width: 100%;height: 1280px;background: #ccc;margin-top: 20px;">test</div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
 import pie from '../echarts/pie'
+import lineCharts from '../echarts/line'
+import { initPieData, initLineData } from '@/utils/common'
+import { mapState,mapMutations } from 'vuex'
 export default {
     name: 'home',
+    computed: mapState(['charts']),
     data(){
         return {
             data: {},
-            id: 'test'
+            id: 'test',
+            data1: {},
+            id1: 'test1',
+            test: 0,
+            debounced: true
         }
     },
-    components: { pie },
+    components: { pie, lineCharts },
     created(){
-        
+        this.data = initPieData({
+            name: 'EPON',
+            data: [
+                {value: 85, name: 'online'},
+                {value: 3, name: 'offline'},
+                {value: 16, name: 'warning'}
+            ]
+        })
+        this.data1 = initLineData({
+            name: 'test1',
+            xAxis: ['00:00', '02:00', '04:00', '06:00', '08:00', '10:00', '12:00'],
+            data: [820, 932, 901, 934, 1290, 1330]
+        })
     },
     mounted(){
-        this.data = {
-            backgroundColor: '#2c343c',
-            title: {
-                text: 'test Pie',
-                left: 'center',
-                top: 20,
-                textStyle: {
-                    color: '#ccc'
-                }
-            },
-            series : [
-                {
-                    type: 'pie',
-                    radius : '55%',
-                    center: ['50%', '50%'],
-                    data: [
-                        {value:335, name:'直接访问'},
-                        {value:310, name:'邮件营销'},
-                        {value:274, name:'联盟广告'},
-                        {value:235, name:'视频广告'},
-                        {value:400, name:'搜索引擎'}
-                    ].sort((a, b) => a.value - b.value)
-                }
-            ]
-        };
+        var _this = this;
+        document.body.onresize = function(){
+            if(_this.debounced){
+                _this.debounced = false;
+                // to do
+                _this.charts.forEach(item =>{
+                    if(typeof item.resize === 'function'){
+                        item.resize();
+                    }
+                })
+                setTimeout(() =>{
+                    _this.debounced = true;
+                }, 300)
+            }
+        }
+    },
+    methods: {
+    },
+    beforeDestroy(){
+        document.body.onresize = null;
     }
 }
 </script>
