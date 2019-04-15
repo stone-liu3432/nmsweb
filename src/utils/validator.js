a439743f7ce6aa7fe11b58f9a7419ef3e9de58a0
stone-liu3432 <liushuang3432@163.com>
2019-04-15 19:48:13 +0800

    添加公用验证规则

---
+++ src/utils/validator.js
@@ -0,0 +1,105 @@
+/* 
+* 表单验证规则，
+* 所有表单验证都应集合在此文件内
+* 验证规则应可以单独引用，也可以作为一个整体导出
+*/
+//  设备名称
+export function validatorDevName(rule, value, callback){
+    if(!value){
+        return callback(new Error('device name'));
+    }
+    callback();
+}
+//  MAC地址
+export function validatorMac(rule, value, callback){
+    if(!value){
+        return callback(new Error('mac'));
+    }
+    callback();
+}
+//  IP地址
+export function validatorIpAddr(rule, value, callback){
+    if(!value){
+        return callback(new Error('ip address'));
+    }
+    callback();
+}
+//  IP掩码
+export function validatorIpMask(rule, value, callback){
+    if(!value){
+        return callback(new Error('ip mask'));
+    }
+    callback();
+}
+//  设备型号
+export function validatorModel(rule, value, callback){
+    if(!value){
+        return callback(new Error('device model'));
+    }
+    callback();
+}
+//  设备类型
+export function validatorClass(rule, value, callback){
+    if(!value){
+        return callback(new Error('class'));
+    }
+    callback();
+}
+//  分组名称
+export function validatorGroupName(rule, value, callback){
+    if(!value){
+        return callback(new Error('group name'));
+    }
+    callback();
+}
+//  用户名
+export function validatorUserName(rule, value, callback){
+    if(!value){
+        return callback(new Error('user name'));
+    }
+    callback();
+}
+//  密码
+export function validatorPassword(rule, value, callback){
+    if(!value){
+        return callback(new Error('password'));
+    }
+    callback();
+}
+//  SNMP community
+export function validatorCommunity(rule, value, callback){
+    if(!value){
+        return callback(new Error('community'));
+    }
+    callback();
+}
+//  网络协议
+export function validatorProtocol(rule, value, callback){
+    if(!value){
+        return callback(new Error('protocol'));
+    }
+    callback();
+}
+//  端口号
+export function validatorPort(rule, value, callback){
+    if(!value){
+        return callback(new Error('port '));
+    }
+    callback();
+}
+const validator = {
+    validatorDevName,
+    validatorMac,
+    validatorIpAddr,
+    validatorModel,
+    validatorClass,
+    validatorGroupName,
+    validatorUserName,
+    validatorPassword,
+    validatorIpMask,
+    validatorCommunity,
+    validatorProtocol,
+    validatorPort,
+}
+
+export default validator;
