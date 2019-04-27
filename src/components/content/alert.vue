b2bb4a4ab9ebd8c405a94c0c3e84a6af0d61c5e5
stone-liu3432 <liushuang3432@163.com>
2019-04-27 15:53:11 +0800

    告警信息

--- src/components/content/alert.vue
+++ src/components/content/alert.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <el-table :data="alertTable" border style="width: 100%; margin-top: 20px;">
-            <el-table-column prop="alarm_id" label="ID" width="80"></el-table-column>
-            <el-table-column prop="source" label="告警源"></el-table-column>
-            <el-table-column prop="macaddr" label="MAC地址"></el-table-column>
-            <el-table-column prop="type" label="类型" width="80"></el-table-column>
-            <el-table-column prop="level" label="告警级别" width="80"></el-table-column>
-            <el-table-column prop="location" label="位置信息"></el-table-column>
-            <el-table-column prop="description" label="描述信息"></el-table-column>
-            <el-table-column prop="timestamp" label="时间"></el-table-column>
-            <div slot="empty"> empty </div>
+            <el-table-column prop="alarm_id" :label="lanMap['alarm_id']" width="80"></el-table-column>
+            <el-table-column prop="source" :label="lanMap['source']"></el-table-column>
+            <el-table-column prop="macaddr" :label="lanMap['macaddr']"></el-table-column>
+            <el-table-column prop="type" :label="lanMap['type']" width="80"></el-table-column>
+            <el-table-column prop="level" :label="lanMap['level']" width="80"></el-table-column>
+            <el-table-column prop="location" :label="lanMap['location']"></el-table-column>
+            <el-table-column prop="description" :label="lanMap['description']"></el-table-column>
+            <el-table-column prop="timestamp" :label="lanMap['timestamp']"></el-table-column>
+            <div slot="empty">{{ lanMap['empty'] }}</div>
         </el-table>
         <el-pagination
             style="float: right;"
@@ -19,17 +19,21 @@
             :page-sizes="pageSizes"
             :page-size="pageSize"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="400"
+            :total="alert.length"
+            v-if="alertTable.length > pageSize"
         ></el-pagination>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import { pageSizes } from '@/utils/common-data'
 export default {
     name: "alert",
+    computed: mapState(['lanMap']),
     data() {
         return {
+            alert: [],
             alertTable: [
                 {
                     alarm_id: 0x1001,
@@ -48,11 +52,40 @@ export default {
 
         }
     },
+    created(){
+        this.getData();
+    },
     methods: {
         handleSizeChange(val) {
             this.pageSize = val;
         },
         handleCurrentChange(val) {
+            var pre = this.pageSize * (this.currentPage - 1);
+            if(pre + this.pageSize > this.alert.length){
+                this.alertTable = this.alert.slice(pre);
+            }else{
+                this.alertTable = this.alert.slice(pre, this.pageSize);
+            }
+        },
+        getData(){
+            this.$http.get('/api/alarm').then(res =>{
+                if(res.data.code === 1){
+                    this.alert = res.data.data;
+                    this.currentPage = 1;
+                    if(this.alert && this.alert.length){
+                        if(this.alert.length > this.pageSize){
+                            this.alertTable = this.alert.slice(this.currentPage - 1, this.pageSize* this.currentPage);
+                        }else{
+                            this.alertTable = this.alert;
+                        }
+                    }
+                }else{
+                    this.alert = [];
+                    this.alertTable = [];
+                }
+            }).catch(err =>{
+
+            })
         }
     },
 };
