990cfcc39dd3a706a9e56df56064eb1d63fda78d
stone-liu3432 <liushuang3432@163.com>
2019-05-15 20:00:39 +0800

    告警页修改显示内容

--- src/components/content/alert.vue
+++ src/components/content/alert.vue
@@ -1,14 +1,34 @@
 <template>
     <div>
-        <el-table :data="alertTable" border style="width: 100%; margin-top: 20px;">
-            <el-table-column prop="alarm_id" :label="langMap['alarm_id']" width="100"></el-table-column>
+        <el-table
+            :data="alertTable"
+            border
+            style="width: 100%; margin-top: 20px;"
+            :row-class-name="tableRowClassName"
+        >
+            <el-table-column
+                prop="alarm_id"
+                :formatter="formatHex"
+                :label="langMap['alarm_id']"
+                width="100"
+            ></el-table-column>
             <el-table-column prop="source" :label="langMap['source']"></el-table-column>
             <el-table-column prop="macaddr" :label="langMap['macaddr']"></el-table-column>
-            <el-table-column prop="type" :label="langMap['type']" width="80"></el-table-column>
-            <el-table-column prop="level" :label="langMap['level']" width="80"></el-table-column>
+            <el-table-column
+                prop="type"
+                :formatter="formatType"
+                :label="langMap['type']"
+                width="100"
+            ></el-table-column>
+            <el-table-column
+                prop="level"
+                :formatter="formatLevel"
+                :label="langMap['level']"
+                width="80"
+            ></el-table-column>
             <el-table-column prop="location" :label="langMap['location']"></el-table-column>
             <el-table-column prop="description" :label="langMap['description']"></el-table-column>
-            <el-table-column prop="timestamp" :label="langMap['timestamp']"></el-table-column>
+            <el-table-column prop="timestamp" :formatter="formatTime" :label="langMap['timestamp']"></el-table-column>
             <div slot="empty">{{ langMap['empty'] }}</div>
         </el-table>
         <el-pagination
@@ -26,57 +46,79 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import { pageSizes } from '@/utils/common-data'
+import { mapState } from "vuex";
+import { pageSizes } from "@/utils/common-data";
+import { ALARM_TYPE, ALARM_LEVEL } from "@/utils/common-data";
+import { formatDate } from "@/utils/common";
 export default {
     name: "alert",
-    computed: mapState(['langMap']),
+    computed: mapState(["langMap"]),
     data() {
         return {
             alert: [],
             alertTable: [],
             currentPage: 1,
             pageSize: 20,
-            pageSizes,
-
-        }
+            pageSizes
+        };
     },
-    created(){
+    created() {
         this.getData();
     },
     methods: {
+        tableRowClassName({ row, rowIndex }) {
+            var level = ALARM_LEVEL[row.level];
+            return level ? `${level}-row` : '';
+        },
         handleSizeChange(val) {
             this.pageSize = val;
         },
         handleCurrentChange(val) {
             var pre = this.pageSize * (this.currentPage - 1);
-            if(pre + this.pageSize > this.alert.length){
+            if (pre + this.pageSize > this.alert.length) {
                 this.alertTable = this.alert.slice(pre);
-            }else{
+            } else {
                 this.alertTable = this.alert.slice(pre, this.pageSize);
             }
         },
-        getData(){
-            this.$http.get('/api/alarm').then(res =>{
-                if(res.data.code === 1){
-                    this.alert = res.data.data;
-                    this.currentPage = 1;
-                    if(this.alert && this.alert.length){
-                        if(this.alert.length > this.pageSize){
-                            this.alertTable = this.alert.slice(this.currentPage - 1, this.pageSize* this.currentPage);
-                        }else{
-                            this.alertTable = this.alert;
+        getData() {
+            this.$http
+                .get("/api/alarm")
+                .then(res => {
+                    if (res.data.code === 1) {
+                        this.alert = res.data.data;
+                        this.currentPage = 1;
+                        if (this.alert && this.alert.length) {
+                            if (this.alert.length > this.pageSize) {
+                                this.alertTable = this.alert.slice(
+                                    this.currentPage - 1,
+                                    this.pageSize * this.currentPage
+                                );
+                            } else {
+                                this.alertTable = this.alert;
+                            }
                         }
+                    } else {
+                        this.alert = [];
+                        this.alertTable = [];
                     }
-                }else{
-                    this.alert = [];
-                    this.alertTable = [];
-                }
-            }).catch(err =>{
-
-            })
+                })
+                .catch(err => {});
+        },
+        formatType(row, column) {
+            return ALARM_TYPE[row[column.property]];
+        },
+        formatLevel(row, column) {
+            return ALARM_LEVEL[row[column.property]];
+        },
+        formatTime(row, column) {
+            return formatDate(Number(row[column.property]));
+        },
+        formatHex(row, column) {
+            var id = row[column.property].toString(16);
+            return `0x${id}`;
         }
-    },
+    }
 };
 </script>
 
