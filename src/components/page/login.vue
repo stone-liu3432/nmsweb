b80169d2c362a3b944a8385b9f3657ea4bf08e33
stone-liu3432 <liushuang3432@163.com>
2019-05-27 20:04:22 +0800

    修改Login页在低分辨率下错位的问题

--- src/components/page/login.vue
+++ src/components/page/login.vue
@@ -115,7 +115,7 @@ export default {
         position: absolute;
         left: 50%;
         top: 50%;
-        transform: translate(-50%, -100%);
+        transform: translate(-50%, -50%);
         padding: 20px;
         border: 1px solid #ddd;
     }
