6c8cbaac88672fd6eaedef5df994df310acac500
stone-liu3432 <liushuang3432@163.com>
2019-05-31 14:52:39 +0800

    修改文件结构

---
+++ src/components/content/deviceMgmt/devDiscover/log-search.vue
@@ -0,0 +1,91 @@
+<template>
+    <div slot="label">
+        <el-collapse>
+            <el-collapse-item title="搜索日志">
+                <el-form inline size="small" :model="logSearch" :rules="rules" ref="logSearch" class="collapse-margin">
+                    <el-form-item label="ip address" label-width="120px" prop="ipaddr">
+                        <el-input v-model="logSearch.ipaddr" placeholder="ip address"></el-input>
+                    </el-form-item>
+                    <el-form-item label="时间范围" label-width="120px" prop="ipaddr">
+                        <el-date-picker v-model="logSearch.stime" type="datetime" placeholder="开始时间"></el-date-picker>-
+                        <el-date-picker v-model="logSearch.etime" type="datetime" placeholder="结束时间"></el-date-picker>
+                        <el-button type="primary" @click="submitData" style="margin-left: 20px;">搜索</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-collapse-item>
+        </el-collapse>
+        <h3>日志详情</h3>
+        <el-table :data="alertTable" border style="width: 100%; margin-top: 20px;">
+            <el-table-column prop="alarm_id" label="ID" width="80"></el-table-column>
+            <el-table-column prop="source" label="告警源"></el-table-column>
+            <el-table-column prop="macaddr" label="MAC地址"></el-table-column>
+            <el-table-column prop="type" label="类型" width="80"></el-table-column>
+            <el-table-column prop="level" label="告警级别" width="80"></el-table-column>
+            <el-table-column prop="location" label="位置信息"></el-table-column>
+            <el-table-column prop="description" label="描述信息"></el-table-column>
+            <el-table-column prop="timestamp" label="时间"></el-table-column>
+            <div slot="empty"> empty </div>
+        </el-table>
+        <el-pagination
+            style="float: right;"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage"
+            :page-sizes="pageSizes"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="400"
+        ></el-pagination>
+    </div>
+</template>
+
+<script>
+import { pageSizes } from '@/utils/common-data'
+import { validatorIpAddr } from '@/utils/validator'
+export default {
+    name: "log-search",
+    data() {
+        return {
+            logSearch: {
+                ipaddr: "",
+                stime: "",
+                etime: ""
+            },
+            rules: {
+                ipaddr: [
+                    { validator: validatorIpAddr, trigger: 'blur' }
+                ]
+            },
+            alertTable: [
+                {
+                    alarm_id: 0x1001,
+                    source: "192.168.100.171",
+                    macaddr: "38:3a:21:20:01:01",
+                    type: 1,
+                    level: 2,
+                    location: 65792,
+                    description: "ONU LINK UP",
+                    timestamp: "2019/01/01 09:09:09"
+                }
+            ],
+            currentPage: 1,
+            pageSize: 20,
+            pageSizes,
+        };
+    },
+    methods: {
+        submitData() {
+            console.log(this.logSearch);
+        },
+        handleSizeChange(val) {
+            this.pageSize = val;
+        },
+        handleCurrentChange(val) {
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/content/deviceMgmt/oltMgmt/devAside.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-menu :default-active="activeName" ref="dev_menu" unique-opened @select="handleMenuClick">
+        <template v-for="(item, index) in menu">
+            <el-menu-item v-if="!item.children" :index="item.name">{{ langMap[item.name] }}</el-menu-item>
+            <el-submenu v-if="item.children" :index="item.name">
+                <template slot="title">{{ langMap[item.name] || item.name }}</template>
+                <el-menu-item
+                    v-for="(_item, _index) in item.children"
+                    :key="_index"
+                    :index="_item.name"
+                >{{ langMap[_item.name] || _item.name }}</el-menu-item>
+            </el-submenu>
+        </template>
+    </el-menu>
+</template>
+
+<script>
+import { mapState, mapMutations } from "Vuex";
+import devMenu from "@/config/devMenu";
+export default {
+    name: "devAside",
+    computed: mapState(["langMap"]),
+    data() {
+        return {
+            menu: devMenu,
+            activeName: "running_status"
+        };
+    },
+    methods: {
+        handleMenuClick(index, indexPath) {
+            this.$emit("menu-change", index);
+        }
+    },
+    watch: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
