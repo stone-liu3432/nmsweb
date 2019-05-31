46b78510e0e2a89693a3d699f4b2e4d092ab850c
stone-liu3432 <liushuang3432@163.com>
2019-05-31 14:48:59 +0800

    修改文件结构

---
+++ src/components/content/taskMgmt/task.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <el-tabs v-model="activeName" type="card">
+            <el-tab-pane :label="langMap['task_mgmt']" name="taskMgmt">
+                <task-mgmt ref="taskMgmt" v-if="activeName === 'taskMgmt'"></task-mgmt>
+            </el-tab-pane>
+            <el-tab-pane :label="langMap['task_temp_mgmt']" name="taskTempMgmt">
+                <task-temp-mgmt v-if="activeName === 'taskTempMgmt'"></task-temp-mgmt>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script>
+import { mapState } from "Vuex";
+import taskMgmt from "./taskMgmt";
+import taskTempMgmt from "./taskTempMgmt";
+export default {
+    name: "task",
+    computed: mapState(["langMap"]),
+    components: { taskMgmt, taskTempMgmt },
+    data() {
+        return {
+            activeName: "taskMgmt"
+        };
+    },
+    methods: {},
+    watch: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
