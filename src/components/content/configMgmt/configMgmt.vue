a250d2a80ef62a58f14102e404bd8be2b045c503
stone-liu3432 <liushuang3432@163.com>
2019-05-31 14:47:25 +0800

    修改文件结构

---
+++ src/components/content/configMgmt/configMgmt.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        config management
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'configMgmt',
+    data(){
+        return {}
+    },
+    created(){
+        this.$message('test');
+        setTimeout(_ =>{
+            this.$message({
+                type: 'warning',
+                message: 'test options'
+            })
+        }, 3000)
+    },
+    methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
