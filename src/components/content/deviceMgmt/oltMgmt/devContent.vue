0948453def848cba5464fa4d627cf630867e5e08
stone-liu3432 <liushuang3432@163.com>
2019-06-03 19:10:20 +0800

    增加远程管理页

--- src/components/content/deviceMgmt/oltMgmt/devContent.vue
+++ src/components/content/deviceMgmt/oltMgmt/devContent.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
         <running-status v-if="showContent === 'running_status'"></running-status>
+        <remote-mgmt v-if="showContent === 'remote_mgmt'"></remote-mgmt>
     </div>
 </template>
 
 <script>
 const runningStatus = () => import(/* webpackChunkName: olt-system */ './system/running-status');
+const remoteMgmt = () => import(/* webpackChunkName: olt-system */ './system/remoteMgmt');
 export default {
     name: 'devContent',
     props: ['showContent'],
-    components: { runningStatus },
+    components: { runningStatus, remoteMgmt },
     data(){
         return {}
     },
