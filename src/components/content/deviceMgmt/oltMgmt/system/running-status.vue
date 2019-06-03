14b298260a9006f618bd52c346fb8cad1780cbd6
stone-liu3432 <liushuang3432@163.com>
2019-06-03 19:09:11 +0800

    设备状态页

--- src/components/content/deviceMgmt/oltMgmt/system/running-status.vue
+++ src/components/content/deviceMgmt/oltMgmt/system/running-status.vue
@@ -1,44 +1,76 @@
 <template>
     <div>
-        <h3 style="margin-left: 10px;">{{ langMap['pon_info'] }}</h3>
+        <h3>{{ langMap['pon_info'] }}</h3>
         <el-row :gutter="10" class="pon-status">
             <template v-for="(item,index) in ponInfo">
                 <el-col :span="3" class="pon-show-detail">
-                    <div>
-                        <img :src="generateSrc('pon', item.status)"></img>
-                    </div>
-                    <div>
-                        <span>{{ port_name[item.port_id] }}</span>
-                        <span>online</span>
-                    </div>
-                    <!-- <div>
-                        <span>registed</span>
-                        <span>12</span>
-                    </div>
-                    <div>
-                        <span>online</span>
-                        <span>11</span>
-                    </div>
-                    <div>
-                        <span>offline</span>
-                        <span>1</span>
-                    </div> -->
+                    <el-popover placement="bottom" trigger="hover" :key="'pon-info' + index">
+                        <div class="pon-info-item">
+                            <span>{{ langMap['registered_onu'] }}:</span>
+                            <span>{{ item.online + item.offline }}</span>
+                        </div>
+                        <div class="pon-info-item">
+                            <span>{{ langMap['online'] }}:</span>
+                            <span>{{ item.online }}</span>
+                        </div>
+                        <div class="pon-info-item">
+                            <span>{{ langMap['offline'] }}:</span>
+                            <span>{{ item.offline }}</span>
+                        </div>
+                        <div slot="reference">
+                            <img :src="generateSrc('pon', item)">
+                            <div style="padding: 3px 0;">
+                                <span>{{ port_name[item.port_id] }}</span>
+                                <span>{{ item.status ? langMap['online'] : langMap['offline'] }}</span>
+                            </div>
+                        </div>
+                    </el-popover>
                 </el-col>
             </template>
         </el-row>
+        <h3>{{ langMap['ge_port_info'] }}</h3>
+        <el-row :gutter="10" class="pon-status">
+            <template v-for="(item,index) in portInfo" v-if="index >= basicInfo.ponports">
+                <el-col :span="3" class="pon-show-detail">
+                    <el-popover
+                        placement="bottom"
+                        width="200"
+                        trigger="hover"
+                        :key="'ge-xge-info' + index"
+                    >
+                        <div class="pon-info-item">
+                            <span>{{ langMap['admin_status'] }}:</span>
+                            <span>{{ item.admin_status ? langMap['enable'] : langMap['disable'] }}</span>
+                        </div>
+                        <div class="pon-info-item">
+                            <span>{{ langMap['link_status'] }}:</span>
+                            <span>{{ item.link_status ? langMap['link_up'] : langMap['link_down'] }}</span>
+                        </div>
+                        <div slot="reference">
+                            <img :src="generateSrc('ge', item)">
+                            <div style="padding: 3px 0;">
+                                <span>{{ port_name[item.port_id] }}</span>
+                                <span>{{ (item.admin_status && item.link_status) ? langMap['online'] : langMap['offline'] }}</span>
+                            </div>
+                        </div>
+                    </el-popover>
+                </el-col>
+            </template>
+        </el-row>
+        <h3>{{ langMap['hw_status'] }}</h3>
         <el-row>
-            <el-col :span="12">
+            <el-col :span="8">
                 <el-row
                     v-for="(item, key) in basicInfo"
                     :key="key"
                     class="text-line"
                     v-if="key !== 'bl_ver' && key !== 'vendor'"
                 >
-                    <el-col :span="8">{{ langMap[key] }}</el-col>
-                    <el-col :span="8">{{ item }}</el-col>
+                    <el-col :span="10">{{ langMap[key] }}</el-col>
+                    <el-col :span="14">{{ item }}</el-col>
                 </el-row>
             </el-col>
-            <el-col :span="12">
+            <el-col :span="16">
                 <el-row :gutter="10">
                     <el-col :span="12">
                         <pie-charts :pie-data="cpuInfo" :id="cpu_id" v-if="cpuInfo.series"></pie-charts>
@@ -60,16 +92,16 @@ export default {
     name: "runningStatus",
     components: { pieCharts },
     computed: {
-        ...mapState(["langMap", "port_name", "dev_ip", 'basicInfo']),
+        ...mapState(["langMap", "port_name", "dev_ip", "basicInfo"])
     },
     data() {
         return {
             cpuInfo: {},
             memoryInfo: {},
-            cpu_id: 'olt-cpu-usde',
-            memory_id: 'olt-memory-used',
-            ponInfo: '',
-            portInfo: ''
+            cpu_id: "olt-cpu-usde",
+            memory_id: "olt-memory-used",
+            ponInfo: "",
+            portInfo: ""
         };
     },
     created() {
@@ -119,44 +151,69 @@ export default {
                 })
                 .catch(err => {});
         },
-        getPon(){
+        getPon() {
             this.$devProxy({
                 devicelist: [this.dev_ip],
                 url: this.$qs({ url: "/board", params: { info: "pon" } }),
                 method: "get"
             })
                 .then(res => {
-                    this.ponInfo = '';
-                    if(res.data.code === 1){
-                        if(res.data.data && res.data.data.length){
+                    this.ponInfo = "";
+                    if (res.data.code === 1) {
+                        if (res.data.data && res.data.data.length) {
                             this.ponInfo = res.data.data;
                         }
                     }
                 })
                 .catch(err => {});
         },
-        getPort(){
+        getPort() {
             this.$devProxy({
                 devicelist: [this.dev_ip],
-                url: this.$qs({ url: "/switch_port", params: { form: "portlist_info" } }),
+                url: this.$qs({
+                    url: "/switch_port",
+                    params: { form: "portlist_info" }
+                }),
                 method: "get"
             })
                 .then(res => {
-                    this.portInfo = '';
-                    if(res.data.code === 1){
-                        if(res.data.data && res.data.data.length){
+                    this.portInfo = "";
+                    if (res.data.code === 1) {
+                        if (res.data.data && res.data.data.length) {
                             this.portInfo = res.data.data;
                         }
                     }
                 })
                 .catch(err => {});
         },
-        generateSrc(type, status, adminstatus){
-            if(type === 'pon'){
-                if(status){
-                    return '/static/pon_online.png';
+        generateSrc(type, item) {
+            if (type === "pon") {
+                return (
+                    "/static" +
+                    (item.status ? "/pon_online.png" : "/pon_offline.png")
+                );
+            }
+            if (type === "ge") {
+                if (item.media === "fiber") {
+                    return (
+                        "/static" +
+                        (item.admin_status
+                            ? item.link_status
+                                ? "/uplink-fiber-blue.png"
+                                : "/uplink-fiber-black.png"
+                            : "/uplink-fiber-black-disable.png")
+                    );
                 }
-                return '/static/pon_offline.png';
+                if (item.media === "copper") {
+                    return (
+                        "/static" +
+                        (item.admin_status
+                            ? item.link_status
+                                ? "/uplink-rj45-blue.png"
+                                : "/uplink-rj45-black.png"
+                            : "/uplink-rj45-disable.png")
+                    );
+                }
             }
         }
     }
@@ -167,18 +224,24 @@ export default {
 .text-line {
     padding: 8px;
 }
-img{
+img {
     width: 32px;
     height: 32px;
 }
-.pon-status{
+.pon-status {
     margin: 30px 0;
 }
-.pon-show-detail{
+.pon-show-detail {
     text-align: center;
-    >div{
-        padding: 3px 0;
-        vertical-align: middle;
+}
+.pon-info-item {
+    padding: 3px 0;
+    > span {
+        display: inline-block;
+        &:first-child {
+            width: 60%;
+            text-align: center;
+        }
     }
 }
 </style>
