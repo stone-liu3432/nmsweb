5d3565144cf5fb62f00a19cad5a6dd3218384b63
stone-liu3432 <liushuang3432@163.com>
2019-06-06 18:25:15 +0800

    添加olt时间管理页

--- src/components/content/deviceMgmt/oltMgmt/devContent.vue
+++ src/components/content/deviceMgmt/oltMgmt/devContent.vue
@@ -5,19 +5,21 @@
         <service-ssh v-if="showContent === 'service'"></service-ssh>
         <olt-alarm v-if="showContent === 'alarm'"></olt-alarm>
         <olt-user-mgmt v-if="showContent === 'user_mgmt'"></olt-user-mgmt>
+        <olt-time-set v-if="showContent === 'time'"></olt-time-set>
     </div>
 </template>
 
 <script>
 const runningStatus = () => import(/* webpackChunkName: 'olt-system' */ './system/running-status');
 const remoteMgmt = () => import(/* webpackChunkName: 'olt-system' */ './system/remoteMgmt');
-const serviceSsh = () => import(/* webpackChunkName: 'olt-system' */'./system/service');
-const oltAlarm = () => import(/* webpackChunkName: 'olt-system' */'./system/oltAlarm');
-const oltUserMgmt = () => import(/* webpackChunkName: 'olt-system'  */'./system/oltUserMgmt');
+const serviceSsh = () => import(/* webpackChunkName: 'olt-system' */ './system/service');
+const oltAlarm = () => import(/* webpackChunkName: 'olt-system' */ './system/oltAlarm');
+const oltUserMgmt = () => import(/* webpackChunkName: 'olt-system'  */ './system/oltUserMgmt');
+const oltTimeSet = () => import(/* webpackChunkName: 'olt-system */ './system/oltTime');
 export default {
     name: 'devContent',
     props: ['showContent'],
-    components: { runningStatus, remoteMgmt, serviceSsh, oltAlarm, oltUserMgmt },
+    components: { runningStatus, remoteMgmt, serviceSsh, oltAlarm, oltUserMgmt, oltTimeSet },
     data(){
         return {}
     },
