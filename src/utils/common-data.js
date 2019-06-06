dbd5dba597a56b480056de0cc9d3b63732264b0c
stone-liu3432 <liushuang3432@163.com>
2019-06-06 13:56:16 +0800

    添加用户级别枚举

--- src/utils/common-data.js
+++ src/utils/common-data.js
@@ -7,36 +7,40 @@ export const SWITCH = {
 
 //  设备状态枚举
 export const STATUS = [
-    'none',
-    'register',
-    'register_failed',
-    'deregister',
-    'unauth',
-    'ctc_discovery',
-    'auth_success',
-    'auth_limit',
-    'auth_fail',
-    'link_lost',
-    'end'
+    "none",
+    "register",
+    "register_failed",
+    "deregister",
+    "unauth",
+    "ctc_discovery",
+    "auth_success",
+    "auth_limit",
+    "auth_fail",
+    "link_lost",
+    "end"
 ];
 //  告警类型
 export const ALARM_TYPE = [
-    'environ',
-    'equipment',
-    'process',
-    'service',
-    'comm',
-    'tca'
-]
+    "environ",
+    "equipment",
+    "process",
+    "service",
+    "comm",
+    "tca"
+];
 //  告警级别
-export const ALARM_LEVEL = [
-    'critical',
-    'major',
-    'minor',
-    'waring'
-]
+export const ALARM_LEVEL = ["critical", "major", "minor", "waring"];
 
 export const MCLASS = {
-    1: 'EPON',
-    2: 'GPON'
-}
+    1: "EPON",
+    2: "GPON"
+};
+//  olt 用户权限级别
+export const USER_LEVEL = [
+    "manu",
+    "diag",
+    "super",
+    "admin",
+    "operator",
+    "common"
+];
