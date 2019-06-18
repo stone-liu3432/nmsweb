b815e72bb400bcc48f619d830ecab64c6905ff76
stone-liu3432 <liushuang3432@163.com>
2019-06-18 12:13:46 +0800

    添加igmp组件

---
+++ src/components/content/deviceMgmt/oltMgmt/protocol/igmp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        igmp
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "Vuex";
+export default {
+    name: 'igmp',
+    computed: {
+        ...mapState(["langMap", "port_name", "basicInfo", "dev_ip"]),
+    },
+    data(){
+        return {}
+    },
+    created(){},
+    methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
